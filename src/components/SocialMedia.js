7915f39a62399744d374c9622501ac99703979c5
Reginald Galang <reguv760@gmail.com>
2018-09-15 20:46:15 -0700

    mediaquery fixes

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -14,28 +14,35 @@ const SocialMedia = () => {
         <h3>SocialMedia: #rocknbox</h3>
         <ul className="socialMedia">
           <li>
-            <SocialIcon
-              url="http://facebook.com/rock-n-box"
-              color="#efefef"
-              style={{ height: 25, width: 25 }}
-            />
-            <p>Follow us on Facebook!</p>
+            <a href="http://facebook.com/rock-n-box" target="_blank">
+              <SocialIcon
+                url="http://facebook.com/"
+                color="#efefef"
+                style={{ height: 25, width: 25 }}
+              />
+              <p>Follow us!</p>
+            </a>
           </li>
           <li>
-            <SocialIcon
-              url="http://twitter.com/rock-n-box"
-              color="#efefef"
-              style={{ height: 25, width: 25 }}
-            />
-            <p>Send us a Tweet!</p>
+            <a href="http://twitter.com/rock-n-box" target="_blank">
+              <SocialIcon
+
+                url="http://twitter.com/"
+                color="#efefef"
+                style={{ height: 25, width: 25 }}
+              />
+              <p>Send a Tweet!</p>
+            </a>
           </li>
           <li>
-            <SocialIcon
-              url="http://instagram.com/rock-n-box"
-              color="#efefef"
-              style={{ height: 25, width: 25 }}
-            />
-            <p>Share your collection!</p>
+            <a href="http://instagram.com/rock-n-box" target="_blank">
+              <SocialIcon
+                url="http://instagram.com"
+                color="#efefef"
+                style={{ height: 25, width: 25 }}
+              />
+              <p>Share your Stuff!</p>            
+            </a>
           </li>
         </ul>
       </div>
