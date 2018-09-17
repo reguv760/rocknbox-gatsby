b726204198cd2b1b5d79083128aa248365dd2d76
Reginald Galang <reguv760@gmail.com>
2018-09-17 08:00:29 -0700

    add smooth-scroll to Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Link from "gatsby-link"
+import Link from 'gatsby-link';
 import logo from './../assets/svg/rocknbox-logo.svg';
 import Login from './Login';
+
+//smooth scroll implementation:::
+if (typeof window !== 'undefined') {
+  // Make scroll behavior of internal links smooth
+  // eslint-disable-next-line global-require
+  require('smooth-scroll')('a[href*="#"]');
+}
 
 const Navigation = () => {
   return (
     <nav>
       <div className="logo">
-        <Link to="/">
+        <Link to="/" exact>
           <img src={logo} alt="rocknbox logo" />
         </Link>
       </div>
@@ -16,16 +23,16 @@ const Navigation = () => {
 
       <ul className="menu">
         <li>          
-          <Link to="/#products">products</Link>
+          <a href="#products">products</a>
         </li>
         <li>
-          <Link to="/#FAQ">FAQ</Link>
+          <a href="#FAQ">FAQ</a>
         </li>
         <li>
-          <Link to="/#About">About</Link>
+          <a href="#About">About</a>
         </li>
         <li>
-          <Link to="/#Contact">Contact</Link>
+          <a href="#Contact">Contact</a>
         </li>
       </ul>
     </nav>
